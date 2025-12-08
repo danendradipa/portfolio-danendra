0350e8d08f978943c5aef72c3f0d014c5ab27398
danendradipa <danendrach@gmail.com>
2025-12-08 13:07:11 +0700

    Add skill and navigation items constants for portfolio

---
+++ src/constant/data.ts
@@ -0,0 +1,74 @@
+export const skillItem = [
+  {
+    imgSrc: "icons/figma.svg",
+    label: "Figma",
+    desc: "Design tool",
+  },
+  {
+    imgSrc: "icons/html5.svg",
+    label: "HTML",
+    desc: "Structure",
+  },
+  {
+    imgSrc: "icons/css3.svg",
+    label: "CSS",
+    desc: "Styling",
+  },
+  {
+    imgSrc: "icons/tailwindcss.svg",
+    label: "TailwindCSS",
+    desc: "CSS Framework",
+  },
+  {
+    imgSrc: "icons/javascript.svg",
+    label: "JavaScript",
+    desc: "Programming Language",
+  },
+  {
+    imgSrc: "icons/typescript.svg",
+    label: "TypeScript",
+    desc: "Programming Language",
+  },
+  {
+    imgSrc: "icons/python.svg",
+    label: "Python",
+    desc: "Programming Language",
+  },
+  {
+    imgSrc: "icons/jupyter.svg",
+    label: "Jupyter Notebook",
+    desc: "Machine Learning",
+  },
+  {
+    imgSrc: "icons/react.svg",
+    label: "React",
+    desc: "Library Web",
+  },
+  {
+    imgSrc: "icons/nodejs.svg",
+    label: "NodeJS",
+    desc: "Runtime Environment",
+  },
+  {
+    imgSrc: "icons/express.svg",
+    label: "ExpressJS",
+    desc: "Node Framework",
+  },
+  {
+    imgSrc: "icons/mongodb.svg",
+    label: "MongoDB",
+    desc: "Database",
+  },
+  {
+    imgSrc: "icons/github.svg",
+    label: "GitHub",
+    desc: "Version Control",
+  },
+];
+
+export const navItems = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Projects", path: "/projects" },
+    { name: "Contact", path: "/contact" },
+  ];
