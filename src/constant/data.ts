b679ae72f424b51592423d1e10223de79fe16fa7
Danendra Dipa Dananjaya <danendrach@gmail.com>
2025-12-12 14:25:47 +0700

    update constant data

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -70,5 +70,6 @@ export const navItems = [
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Projects", path: "/projects" },
+    { name: "Blogs", path: "/blogs" },
     { name: "Contact", path: "/contact" },
   ];
