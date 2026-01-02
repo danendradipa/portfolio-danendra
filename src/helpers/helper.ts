13855529ab2a98a183d7aac51af84e2477def12b
Danendra Dipa Dananjaya <danendrach@gmail.com>
2026-01-02 16:37:12 +0700

    Add formatDate function for date formatting utility

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -14,3 +14,13 @@ export const deleteFileFromUrl = async (imageUrl: string) => {
     console.error("Error helper delete:", err);
   }
 };
+
+export const formatDate = (dateString: string): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const date = new Date(dateString);
+  return date.toLocaleDateString(undefined, options);
+};
