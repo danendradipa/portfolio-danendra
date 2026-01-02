95f25cdf7b40de198ec40a08d6a049b3084eb9aa
Danendra Dipa Dananjaya <danendrach@gmail.com>
2026-01-02 16:38:13 +0700

    Add BlogPage component to render blog section

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,11 @@
+import BlogSection from "@/components/BlogSection";
+
+const BlogPage = () => {
+  return (
+    <section className="pt-18 pb-20 px-8 bg-zinc-100 dark:bg-zinc-900">
+      <BlogSection />
+    </section>
+  );
+};
+
+export default BlogPage;
