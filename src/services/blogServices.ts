404fc1e5ee36265260e3763167d95f1debbe6599
Danendra Dipa Dananjaya <danendrach@gmail.com>
2026-01-14 13:29:02 +0700

    Refactor getBlogs and getBlogBySlug functions to include unpublished blogs option

--- src/services/blogServices.ts
+++ src/services/blogServices.ts
@@ -3,14 +3,33 @@ import { Blog, BlogInput } from "@/types/Blog";
 import { deleteFileFromUrl } from "@/helpers/helper";
 
 // GET ALL
-export const getBlogs = async () => {
-  const { data, error } = await supabase
+export const getBlogs = async (includeUnpublished: boolean = false) => {
+  let query = supabase
     .from('blogs')
     .select('*')
     .order('created_at', { ascending: false });
   
+  if (!includeUnpublished) {
+    query = query.eq('is_published', true);
+  }
+  
+  const { data, error } = await query;
+  
   if (error) throw error;
   return data as Blog[];
+};
+
+// GET BY SLUG
+export const getBlogBySlug = async (slug: string) => {
+  const { data, error } = await supabase
+    .from('blogs')
+    .select('*')
+    .eq('slug', slug)
+    .eq('is_published', true)
+    .single();
+  
+  if (error) throw error;
+  return data as Blog;
 };
 
 // UPLOAD IMAGE
