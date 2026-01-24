ba21be2c853af849919b716ce5d219804b03fb10
Danendra Dipa Dananjaya <danendrach@gmail.com>
2026-01-24 20:01:29 +0700

    Add additional remote pattern for Unsplash images in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        port: "",
+        pathname: "/**",
+      }
     ],
   },
 };
