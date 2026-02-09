f08262a81f5f18b13622c6fbd285cff5c6803ff5
Danendra Dipa Dananjaya <danendrach@gmail.com>
2026-02-09 15:19:35 +0700

    Update metadata in page.tsx for improved SEO and clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Metadata } from "next";
 import HomePage from "@/components/HomePage";
 
+export const metadata: Metadata = {
+  title: "Home",
+  description:
+    "Welcome to Danendra Dipa's portfolio. Frontend Developer and Machine Learning enthusiast building digital products with precision and purpose.",
+};
+
 export default function Home() {
-  return (
-      <HomePage />
-  );
+  return <HomePage />;
 }
